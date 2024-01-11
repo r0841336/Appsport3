c1d3fb2626b1baa639e3963b6d6099ca79c0160a
Dante <r0841336@student.thomasmore.be>
2024-01-11 11:52:47 +0100

    final commit

--- sport3app/schermen/Winkelwagen.js
+++ sport3app/schermen/Winkelwagen.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-
-
 const CartScreen = ({ route }) => {
   const { cartItems } = route.params;
+  const productsData = [
+
+{ id: 225, title: 'The joker gloves', price: 120 },
+{ id: 73, title: 'Grip boost gloves peace sign', price: 50 },
+{ id: 70, title: 'the american mouthguard', price: 22 },
+{ id: 44, title: 'Nike alpha drip cleats', price: 270 },
+{ id: 41, title: 'Battle gloves proud of america', price: 45.47 },
+{ id: 36, title: 'Wilson american football ball', price: 99.99 },
+{ id: 33, title: 'Riddle speedflex 5Gen', price: 344.9 },
+{ id: 29, title: 'Girdle pants', price: 13.56 },
+{ id: 3, title: 'Xenix pro football helm', price: 12.88 },
+
+  ];
+
+  const getCartItemData = (productId) => {
+    return productsData.find((product) => product.id === productId);
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Winkelwagentje</Text>
       <FlatList
-    data={cartItems}
-    keyExtractor={(item) => item.id.toString()}
-    renderItem={({ item }) => (
-        <View style={styles.cartItem}>
-            <Text>{item.title}</Text>
-            <Text>{item.price}</Text>
-        </View>
-    )}
-/>
+        data={cartItems}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => {
+          const productData = getCartItemData(item.id);
+
+          return (
+            <View style={styles.cartItem}>
+              <Text>{productData.title}</Text>
+              <Text>{productData.price}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
